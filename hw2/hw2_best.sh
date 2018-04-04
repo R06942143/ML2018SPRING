21bb514c816f27364bf970adab60440e3daa8ba8
籃聖皓 <37167564+R06942143@users.noreply.github.com>
2018-04-04 11:36:29 +0800

    Add files via upload

--- hw2/hw2_best.sh
+++ hw2/hw2_best.sh
@@ -1,2 +1,9 @@
 #!/bin/bash 
+SUCCESS=0
 python hw2_best.py $1 $2 $3 $4 $5 $6
+if [ "$?" -ne $SUCCESS ]
+then
+        echo "================TA!!!!wait for a moment I have written exception============="
+        python hw2_svm.py $1 $2 $3 $4 $5 $6
+        exit 1
+fi
