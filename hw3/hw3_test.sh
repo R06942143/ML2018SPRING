6622e3470ddae13c2a9953f5b6f58d367b24fd39
籃聖皓 <37167564+R06942143@users.noreply.github.com>
2018-04-17 22:30:02 +0800

    Update hw3_test.sh

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -3,3 +3,5 @@ wget -O public.h5py 'https://www.dropbox.com/s/k5m08ba3qtibk0d/-257-0.70.h5py?dl
 wget -O private.h5py 'https://www.dropbox.com/s/55vsz7178r02qs5/-209-0.69.h5py?dl=1'
 
 python predict.py $1 $2 $3
+
+#! $3 = 'publi'c or 'private' 
