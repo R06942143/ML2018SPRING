9f2118f3490b57f731a604e0ba39625f89650039
籃聖皓 <37167564+R06942143@users.noreply.github.com>
2018-04-26 10:20:38 +0800

    Add files via upload

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,7 +1,7 @@
 wget -O public.h5py 'https://www.dropbox.com/s/k5m08ba3qtibk0d/-257-0.70.h5py?dl=1'
 
 wget -O private.h5py 'https://www.dropbox.com/s/55vsz7178r02qs5/-209-0.69.h5py?dl=1'
+sed -i -e 's/, "amsgrad": false/                  /g' 'public.h5py'
+sed -i -e 's/, "amsgrad": false/                  /g' 'private.h5py'
 
 python predict.py $1 $2 $3
-
-#! $3 = 'public' or 'private' 
